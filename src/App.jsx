141a1c667e042bda09f2da124445ae130800a8da
NicolasEsmite <nicolasemanuelesmitevaldez@gmail.com>
2024-06-04 19:47:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div className="w-full h-5 bg-red-400">
+      <h1>Hola jamon</h1>
+    </div>
+  );
+}
+
+export default App;
